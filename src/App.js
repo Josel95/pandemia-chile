0665e829d323ab1d713462de8762a287e79f293d
Josel95 <jose.lopezh95@gmail.com>
2021-01-18 21:12:36 +0000

    Separated styles from App.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,12 @@
 import React from 'react';
 
-import styled from 'styled-components'
+import { ViewStyled, CurrentComuna, NearbyComunas, NearbyComunasTitle } from './styles'
 
 import { Comuna } from './components/Comuna/Comuna'
 
 import { Paso } from './components/Paso/Paso'
 
 import { ComunaList } from './components/ComunaList/ComunaList'
-
-const ViewStyled = styled.View`
-    display: flex;
-    align-items: center;
-    padding-top: 50px;
-    height: 100%;
-    /* background-color: salmon; */
-`
-
-const CurrentComuna = styled.View`
-    flex: 30%;
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    /* background-color: purple; */
-`
-
-const NearbyComunas = styled.View`
-    flex: 70%;
-    width: 100%;
-    display: flex;
-    align-items: center;
-    /* background-color: pink; */
-`
-
-const NearbyComunasTitle = styled.Text`
-    font-size: 27px;
-    padding: 15px 0; 
-`
 
 export default function App() {
     return (
---
+++ src/styles.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+
+export const ViewStyled = styled.View`
+    display: flex;
+    align-items: center;
+    padding-top: 50px;
+    height: 100%;
+    /* background-color: salmon; */
+`
+
+export const CurrentComuna = styled.View`
+    flex: 30%;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    /* background-color: purple; */
+`
+
+export const NearbyComunas = styled.View`
+    flex: 70%;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    /* background-color: pink; */
+`
+
+export const NearbyComunasTitle = styled.Text`
+    font-size: 27px;
+    padding: 15px 0; 
+`
