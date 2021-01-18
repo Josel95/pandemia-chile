53645110595ee51e36b3b3b9b0698bba6024f3c9
Josel95 <jose.lopezh95@gmail.com>
2021-01-18 20:25:34 +0000

    Changed main screen styles

--- src/App.js
+++ src/App.js
@@ -13,15 +13,43 @@ const ViewStyled = styled.View`
     align-items: center;
     padding-top: 50px;
     height: 100%;
+    /* background-color: salmon; */
+`
+
+const CurrentComuna = styled.View`
+    flex: 30%;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    /* background-color: purple; */
+`
+
+const NearbyComunas = styled.View`
+    flex: 70%;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    /* background-color: pink; */
+`
+
+const NearbyComunasTitle = styled.Text`
+    font-size: 27px;
+    padding: 15px 0; 
 `
 
 export default function App() {
-  return (
-    <ViewStyled>
-      <Comuna comuna={'San Miguel'} />
-      <Paso paso={2} />
+    return (
+        <ViewStyled>
+            <CurrentComuna>
+                <Comuna comuna={'San Miguel'} />
+                <Paso paso={2} />
+            </CurrentComuna>
 
-      <ComunaList/>
-    </ViewStyled>
-  );
+            <NearbyComunas>
+                <NearbyComunasTitle>Comunas cercanas</NearbyComunasTitle>
+                <ComunaList />
+            </NearbyComunas>
+        </ViewStyled>
+    );
 }
