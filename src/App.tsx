25ec4b5b9c098f522d6d3f5004f95766cc574a69
josel95 <jose.lopezh95@gmail.com>
2021-03-05 18:43:16 -0300

    Estructura principal

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+import { SafeAreaView, StatusBar } from 'react-native';
+
+const App = () => {
+  return (
+    <Fragment>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+          
+        </SafeAreaView>
+    </ Fragment>
+  );
+};
+
+export default App;
