15098e43344daece2dfec39934701f23c28e46dd
josel95 <jose.lopezh95@gmail.com>
2021-03-07 01:54:55 -0300

    Solicita permiso para utilizar la ubicacion

--- src/hooks/useLocation.tsx
+++ src/hooks/useLocation.tsx
@@ -1,10 +1,31 @@
 import { useState, useEffect } from 'react'
 
+import { PermissionsAndroid } from 'react-native'
+
 import Geolocation from '@react-native-community/geolocation'
 
 import { getComuna } from '../geocoding/getComuna'
 
 import { Coords } from '../types/Coords'
+
+const requestLocationPermission = async () => {
+    const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+            title: 'Pandemia Chile',
+            message: 'La aplicacion Pandemia Chile requiere acceso a tu ubicación para ofrecerte la mejor experiencia.',
+            buttonNeutral: 'Preguntar mas tarde.',
+            buttonNegative: 'Cancelar',
+            buttonPositive: 'Aceptar',
+        }
+    )
+
+    return granted === PermissionsAndroid.RESULTS.GRANTED
+}
+
+const checkLocationPermission = async () => {
+    return await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION)
+}
 
 export const useLocation = () => {
     const [coords, setCoords] = useState<Coords | undefined>()
@@ -13,31 +34,46 @@ export const useLocation = () => {
 
     const [error, setError] = useState<string | undefined>()
 
+    const [permissionGranted, setPermissionGranted] = useState(false)
+
     useEffect(() => {
-        Geolocation.setRNConfiguration({authorizationLevel: 'whenInUse', skipPermissionRequests: false})
-
-        const watchId = Geolocation.watchPosition(
-            position => {
-                setCoords({
-                    longitude: position.coords.longitude,
-                    latitude: position.coords.latitude,
-                })
-            },
-            error => {
-                console.log('Ha ocurrido un error obteniendo la posicion')
-            },
-            {
-                enableHighAccuracy: true,
-                timeout: 15000,
-                maximumAge: 10000,
-                useSignificantChanges: true
+        (async () => {
+            const permission = await checkLocationPermission()
+            if(!permission) {
+                const permission = await requestLocationPermission()
+                setPermissionGranted(permission)
             }
-        )
-
-        return () => {
-            Geolocation.clearWatch(watchId)
-        }
+            setPermissionGranted(permission)
+        })()
     }, [])
+
+    useEffect(() => {
+        console.log(permissionGranted)
+        if(permissionGranted){
+            const watchId = Geolocation.watchPosition(
+                position => {
+                    setCoords({
+                        longitude: position.coords.longitude,
+                        latitude: position.coords.latitude,
+                    })
+                },
+                error => {
+                    console.log(error)
+                    console.log('Ha ocurrido un error obteniendo la posicion')
+                },
+                {
+                    enableHighAccuracy: true,
+                    timeout: 15000,
+                    maximumAge: 10000,
+                    useSignificantChanges: true
+                }
+            )
+    
+            return () => {
+                Geolocation.clearWatch(watchId)
+            }
+        }
+    }, [permissionGranted])
 
     useEffect(() => {
         (async () => {
