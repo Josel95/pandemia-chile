736a868f0a2566ae7761b6a780782da203b659cd
josel95 <jose.lopezh95@gmail.com>
2021-03-07 12:42:25 -0300

    Agregado resultado de busqueda

---
+++ src/components/SearchItem/SearchItem.tsx
@@ -0,0 +1,37 @@
+import React, { FC } from 'react'
+
+import { ListItem } from 'react-native-elements'
+
+import { useNavigation } from '@react-navigation/native'
+
+import { styles } from './styles'
+import { Text } from 'react-native'
+
+interface Props {
+    id: string
+    name: string
+    searchTerm: string
+}
+
+export const SearchItem: FC<Props> = ({ id, name, searchTerm }) => {
+
+    const navigation = useNavigation()
+
+    const handlePress = () => {
+        navigation.navigate('ComunaDetail', { id })
+    }
+
+    const restName = name.substr(searchTerm.length, name.length)
+
+    return (
+        <ListItem key={id} bottomDivider onPress={handlePress}>
+            <ListItem.Content>
+                <Text>
+                    <Text style={[styles.text, styles.bold]}>{searchTerm}</Text>
+                    <Text style={styles.text}>{restName}</Text>
+                </Text>
+            </ListItem.Content>
+            <ListItem.Chevron />
+        </ListItem>
+    )
+}
--- src/screens/SearchScreen/SearchScreen.tsx
+++ src/screens/SearchScreen/SearchScreen.tsx
@@ -1,18 +1,32 @@
 import React, { FC } from 'react'
 
-import { Text } from 'react-native'
+import { Text, FlatList } from 'react-native'
+
+import { SearchItem } from '../../components/SearchItem'
 
 import { State } from '../../types/State'
 
 import { useSelector } from 'react-redux'
 
+import { comunaCodes } from '../../comunaCodes'
+
 import { styles } from './styles'
 
 export const SearchScreen: FC = () => {
-
     const searchTerm = useSelector((state: State) => state.search.searchTerm)
 
+    const filteredEntries = Object.entries(comunaCodes).filter(entrie => {
+        const comunaName: string = entrie[1]
+        return comunaName.toLowerCase().startsWith(searchTerm.toLowerCase())
+    })
+
     return (
-        <Text>{searchTerm}</Text>
+        <FlatList 
+            data={filteredEntries}
+            renderItem={(entrie) => <SearchItem id={entrie.item[0]} name={entrie.item[1]} searchTerm={searchTerm}/>}
+            initialNumToRender={15}
+            ListEmptyComponent={<Text>No se han encontrado resultados</Text>}
+            keyExtractor={(entrie) => entrie[0]}
+        />
     )
 }
